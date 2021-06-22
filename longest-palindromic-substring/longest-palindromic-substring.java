f9c8881cbb6be956d2083074c013e79430d9b876
yash19480 <yash19480@iiitd.ac.in>
2021-06-23 01:33:37 +0530

    Time: 263 ms (23.76%), Space: 50.2 MB (9.62%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,64 +3,48 @@ class Solution {
         int n = s.length();
         int[][] dp = new int[n+1][n+1];
         
-        for(int len=0; len<n; len++){
+        int stIdx = 0;
+        int endIdx = 0;
+        int maxLen = 1;
+        
+        
+        for(int i=0; i<n; i++){
+            dp[i][i] = 1;
+        }
+        for(int len = 1; len < n; len++){
             int x = 0;
-            int y = x + len;
-            // System.out.println(x + " " + y);
-            while(x < n && y < n){
-                // System.out.println("x and y" + x + " " + y);
-                if(x == y){
-                    dp[x][y] = 1;
-                }
-                else{
-                    if(x == y-1){
-                        if(s.charAt(x) == s.charAt(y)){
-                            dp[x][y] = 1;
+            int y = len;
+            
+            while(x<n && y<n){
+                if(s.charAt(x) == s.charAt(y)){
+                    if(x+1 <= y-1){
+                        if(dp[x+1][y-1] > 0){
+                            dp[x][y] = dp[x+1][y-1] + 2;
+                            if(maxLen < dp[x][y]){
+                                maxLen = dp[x][y]; 
+                                stIdx = x;
+                                endIdx = y;
+                            }
                         }
                         else{
                             dp[x][y] = 0;
                         }
                     }
                     else{
-                        if(s.charAt(x) == s.charAt(y)){
-                            dp[x][y] = dp[x+1][y-1];
-                        }
-                        else{
-                            dp[x][y] = 0;
+                        dp[x][y] = 2;
+                        if(maxLen < dp[x][y]){
+                            maxLen = dp[x][y];
+                            stIdx = x; 
+                            endIdx = y;
                         }
                     }
-                }
-                x++; y++;
-            }
-        }
-        
-        int stIdx = 0;
-        int endIdx = 0;
-        int maxLen = 0;
-        
-        for(int len=0; len<n; len++){
-            int x = 0;
-            int y = x + len;
-            
-            while(x < n && y < n){
-                if(x == y){
-                    stIdx = x;
-                    endIdx = x;
-                    maxLen = 1;
                 }
                 else{
-                    if(dp[x][y] == 1){
-                        if(maxLen < (y-x+1)){
-                            stIdx = x;
-                            endIdx = y;
-                            maxLen = y-x+1;
-                        }
-                    }
+                    dp[x][y] = 0;
                 }
                 x++; y++;
             }
         }
-        System.out.println(stIdx + " " + endIdx + " " + maxLen);
         return s.substring(stIdx, endIdx+1);
     }
 }
