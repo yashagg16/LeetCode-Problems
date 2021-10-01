5df0d12de28ad280297cfc751155115fb2de22f7
yash19480 <yash19480@iiitd.ac.in>
2021-10-01 11:45:10 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (72.38%) - LeetHub

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -3,19 +3,24 @@ class Solution {
         int st = 0;
         int end = nums.length-1;
         
-        while(st+1 < end){
-            int mid = (st+end)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] < target){
-                st = mid;
+        int mid;
+        
+        while(st + 1 < end){
+            mid = (st + end)/2;
+            
+            if(nums[mid] == target){
+                return mid;
             }
-            else{
+            else if(nums[mid] > target){
                 end = mid;
+            }
+            else {
+                st = mid;
             }
         }
         
         if(nums[st] == target) return st;
-        if(nums[end] == target) return end;
+        if(nums[end] == target) return  end;
         
         return -1;
     }
