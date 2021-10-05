ab0528291795136e60e3e3fee03211456464c569
yash19480 <yash19480@iiitd.ac.in>
2021-10-05 10:03:00 +0530

    Time: 3 ms (35.51%), Space: 53.1 MB (24.69%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] arr = new int[nums.length];
         int n = nums.length;
+        
+        int[] ans = new int[n];
+        
         int i=0;
         int j=n-1;
         int k=n-1;
-        while(i < n && j >= 0 && i <= j){
-            // System.out.println(i + " " + j + " " + k);
-            if(Math.abs(nums[i]) <= Math.abs(nums[j])){
-                arr[k--] = nums[j--];
+        while(i <= j){
+            if(Math.abs(nums[i]) > Math.abs(nums[j])){
+                ans[k] = nums[i];
+                i++;
+                k--;
             }
             else{
-                arr[k--] = nums[i++];
+                ans[k] = nums[j];
+                k--;
+                j--;
             }
         }
         
-        for( i=0; i<n; i++){
-            arr[i] *= arr[i];
+        for(i=0; i<n; i++){
+            ans[i] *= ans[i];
         }
         
-        return arr;
+        return ans;
     }
 }
