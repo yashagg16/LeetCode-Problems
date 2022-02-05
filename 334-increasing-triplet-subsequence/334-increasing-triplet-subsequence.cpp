d518c9a28e75b9f7542c6ff689bbaa373e2fa9de
yash19480 <yash19480@iiitd.ac.in>
2022-02-05 20:26:39 +0530

    Time: 52 ms (96.83%), Space: 61.4 MB (99.04%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -2,41 +2,16 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         int n = nums.size();
+        
         if(n < 3) return false;
         
-        vector<int> maxRight(n, 0);
-        maxRight[n-1] = nums[n-1];
+        int min_val = INT_MAX;
+        int max_val = INT_MAX;
         
-        for(int i=n-2; i>=0; i--){
-            maxRight[i] = max(maxRight[i+1], nums[i+1]);
-        }
-        
-        set<int> s;
-        s.insert(nums[0]);
-        for(int i=1; i<n-1; i++){
-            if(nums[i] >= maxRight[i]){
-                s.insert(nums[i]);
-                continue;
-            }
-            auto x = s.lower_bound(nums[i]);
-            // cout << *x << " " << nums[i] << " " << maxRight[i] << endl;
-            if(x == s.begin()){
-                if(*x < nums[i]){
-                    return true;
-                }
-                else{
-                    s.insert(nums[i]);
-                    continue;
-                }
-            }
-            else if(x == s.end()){
-                return true;
-            }
-            else{
-                --x;
-                if(*x < nums[i]) return true;
-                else return false;
-            }
+        for(int i=0; i<n; i++){
+            if(nums[i] <= min_val) min_val = nums[i];
+            else if(nums[i] <= max_val) max_val = nums[i];
+            else return true;
         }
         
         return false;
