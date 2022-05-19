dec89ace422ecf0a6762694ceb68f49180f9e09a
yash19480 <yash19480@iiitd.ac.in>
2022-05-19 15:42:25 +0530

    Time: 51 ms (40.91%), Space: 12.2 MB (84.85%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     void dfs(vector<vector<char>>& grid, int i, int j, int n, int m){
-        // cout << i << " " << j << endl;
-        if(i < 0 || j < 0) return;
-        if(i >= n || j >= m) return;
-        if(grid[i][j] == '0') return;
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') return;
         
         grid[i][j] = '0';
         dfs(grid, i+1, j, n, m);
-        dfs(grid, i, j+1, n, m);
         dfs(grid, i-1, j, n, m);
+        dfs(grid, i, j+1, n, m);
         dfs(grid, i, j-1, n, m);
     }
     int numIslands(vector<vector<char>>& grid) {
-        
+        int cnt = 0;
         int n = grid.size();
         int m = grid[0].size();
-        
-        int cnt = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == '1'){
@@ -26,6 +21,7 @@ public:
                 }
             }
         }
+        
         return cnt;
     }
 };
