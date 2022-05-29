d5ebf4ff6a523afffff87e7a8effa9390348cd27
yash19480 <yash19480@iiitd.ac.in>
2022-05-29 22:24:05 +0530

    Time: 4 ms (31.98%), Space: 5.9 MB (35.17%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<bool> dp(n+1, false);
+        dp[1] = false; 
+        
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<i; j++){
+                if(i%j == 0){
+                    dp[i] = dp[i] || (!dp[i-j]);
+                }
+            }
+        }
+        
+        return dp[n];
+    
+    }
+};
