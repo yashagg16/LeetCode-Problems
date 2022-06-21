335f0b1889cfd5b235f78e396a6491951b67096e
yash19480 <yash19480@iiitd.ac.in>
2022-06-21 23:09:25 +0530

    Time: 218 ms (5.02%), Space: 13.8 MB (94.04%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,12 +1,37 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        dp = [False for i in range(n+1)]
+        if (n == 1) : 
+            return False
+        lst = [0,0]
         
-        for i in range(2, n+1) : 
-            for j in range(1, i) : 
+        for i in range(2, n+1) :
+            res = False
+            for j in range(1, i) :
                 if(i%j == 0) : 
-                    # print(i, j)
-                    dp[i] = dp[i] or (not(dp[i-j]))
-                    
-        return dp[n]
+                    res = res or (not(lst[i-j]))
+            lst.append(res)
+            
+        return lst[n]
         
+        
+# 0 < x < n  and n%x == 0
+#     n = n-x
+    
+# lose the game if not possible 
+# true = Alice wins
+# false = Alice loses
+
+# f(1)
+# false
+
+# f(2)
+# !f(1)
+# true 
+
+# f(3)
+# !f(2)
+# false
+
+# f(4)
+# !f(3) || !f(2)
+# true
