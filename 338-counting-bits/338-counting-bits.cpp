e0ef7d0050e55cd77eebde28af8ea365d28e443b
yash19480 <yash19480@iiitd.ac.in>
2022-06-21 23:29:28 +0530

    Time: 9 ms (48.55%), Space: 7.9 MB (47.55%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp (n+1, 0);
+        
+        if(n == 0) return dp;
+        
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) dp[i] = dp[i/2] + dp[i%2];
+        
+        return dp;
+    }
+};
