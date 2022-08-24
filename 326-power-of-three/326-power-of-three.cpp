48ce3123688a75e4390b9fa00dfb15225e8634db
Yash Aggarwal <yash19480@iiitd.ac.in>
2022-08-24 09:46:13 +0530

    Time: 31 ms (61.16%), Space: 5.9 MB (29.00%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0) return false;
+        
+        while(n > 1 && n%3 == 0){
+            n = n/3;
+        }
+        
+        if(n == 1) return true;
+        
+        return false;
+    }
+};
